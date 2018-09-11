cdd032705d96d6d43efbee46645cba1ef54f44c4
Frioa <916378508@qq.com>
2018-09-11 10:21:21 +0800

    Initial commit

---
+++ Broadcast/src/main/java/com/example/q9163/firstcode/BroadcastTest.java
@@ -0,0 +1,13 @@
+package com.example.q9163.firstcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BroadcastTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast_test);
+    }
+}
