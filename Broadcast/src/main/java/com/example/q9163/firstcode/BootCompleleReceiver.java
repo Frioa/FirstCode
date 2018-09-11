763865e2cb0b9d2cb616b2924aaf0414e40aec1d
Frioa <916378508@qq.com>
2018-09-11 11:02:52 +0800

    静态广播注册 接受开机的广播

---
+++ Broadcast/src/main/java/com/example/q9163/firstcode/BootCompleleReceiver.java
@@ -0,0 +1,19 @@
+package com.example.q9163.firstcode;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleleReceiver extends BroadcastReceiver {
+    public BootCompleleReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "Boot complete ",Toast.LENGTH_SHORT).show();
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
