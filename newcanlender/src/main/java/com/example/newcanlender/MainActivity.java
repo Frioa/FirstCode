8642277815688da2c5a98fae1d9d21436202b65a
Frioa <916378508@qq.com>
2018-09-13 17:41:20 +0800

    自定义View (日历)           自定义控件响应事件

--- newcanlender/src/main/java/com/example/newcanlender/MainActivity.java
+++ newcanlender/src/main/java/com/example/newcanlender/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.newcanlender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements NewCanlender.NewCalenderListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NewCanlender canlender = (NewCanlender) findViewById(R.id.newCalender);
+        canlender.lister = this;
+        String s = "abc";
+        change(s);
+        Log.d("Main", "onCreate: "+s);
+    }
+    void change(String s){
+        s = "abc`1";
+    }
+
+    @Override
+    public void onItemLongPress(Date day) {
+        DateFormat df = SimpleDateFormat.getDateInstance();
+        Toast.makeText(this, df.format(day), Toast.LENGTH_SHORT).show();
     }
 }
--- newcanlender/src/main/java/com/example/newcanlender/NewCanlender.java
+++ newcanlender/src/main/java/com/example/newcanlender/NewCanlender.java
@@ -1,14 +1,17 @@
 package com.example.newcanlender;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -29,6 +32,9 @@ public class NewCanlender extends LinearLayout {
     private GridView gridView;
 
     private Calendar curDate = Calendar.getInstance();
+    private String display;
+
+    public NewCalenderListener lister;
 
     public NewCanlender(Context context) {
         super(context);
@@ -36,18 +42,32 @@ public class NewCanlender extends LinearLayout {
 
     public NewCanlender(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initControl(context);
+        initControl(context, attrs);
     }
 
     public NewCanlender(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initControl(context);
+        initControl(context, attrs);
     }
-    private void initControl(Context context)
+    private void initControl(Context context, AttributeSet attr)
     {
         bindControl(context);// 查找控件
         bindControlEvent();// 点击事件
+
+
+        TypedArray ta = getContext().obtainStyledAttributes(attr, R.styleable.NewCanlender);
+        try{
+             String format = ta.getString(R.styleable.NewCanlender_dateFormat);
+            display = format;
+            if (display == null){
+                display = "MMM yyyy";
+            }
+         }finally {
+            ta.recycle();
+        }
+
         renderCalender();
+
     }
 
 
@@ -78,7 +98,8 @@ public class NewCanlender extends LinearLayout {
     }
     // 日历控件的绘制
     private void renderCalender(){
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat(display);
+        Log.d("SimpleDateFormat", "renderCalender: "+display);
         textdate.setText(sdf.format(curDate.getTime()));
 
         ArrayList<Date> cells = new ArrayList<>();
@@ -95,6 +116,19 @@ public class NewCanlender extends LinearLayout {
         }
 
         gridView.setAdapter(new CalenderAdapter(getContext(),cells ));
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (lister == null){
+                    return false;
+                }else{
+                    lister.onItemLongPress((Date) adapterView.getItemAtPosition(i));
+                    return true;
+                }
+
+
+            }
+        });
     }
 
     private class CalenderAdapter extends ArrayAdapter<Date>{
@@ -136,8 +170,15 @@ public class NewCanlender extends LinearLayout {
                 ((Calender_day_textview)convertView).setTextColor(Color.parseColor("#ff0000"));
                 ((Calender_day_textview)convertView).isToday = true;
             }
+
+
             return convertView;
         }
+    }
+
+    public interface NewCalenderListener
+    {
+        void onItemLongPress(Date day);
     }
 
 }
